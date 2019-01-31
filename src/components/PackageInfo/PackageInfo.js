928c836a3bbf8f587c6d3e62dd27607b0d754c92
Ken Crites <kenrcrites@gmail.com>
2019-01-31 23:28:27 +0100

    Initial Stats file

--- src/components/PackageInfo/PackageInfo.js
+++ src/components/PackageInfo/PackageInfo.js
@@ -10,59 +10,64 @@ const sessionsTotal = 11;
 const  PackageInfo = () => {
 	return (
 		<div>
-<article class="pa3 pa5-ns" data-name="slab-stat">
+<article className="pa1 pa5-ns" data-name="slab-stat">
   <h1>{name}</h1>
-  <dl class="dib mr5">
-    <dd class="f6 f5-ns b ml0">Closed Issues</dd>
-    <dd class="f3 f2-ns b ml0">1,024</dd>
+  <dl className="dib mr5">
+   	<dd className="f6 f5-ns b ml0">Sessions Used</dd>
+    <dd className="f3 f2-ns b ml0">{sessionsUsed}</dd>
   </dl>
-  <dl class="dib mr5">
-    <dd class="f6 f5-ns b ml0">Open Issues</dd>
-    <dd class="f3 f2-ns b ml0">993</dd>
+  <dl className="dib mr5">
+     <dd className="f6 f5-ns b ml0">Sessions Left</dd>
+     <dd className="f3 f2-ns b ml0">{sessionsTotal-sessionsUsed}</dd>
   </dl>
-  <dl class="dib mr5">
-    <dd class="f6 f5-ns b ml0">Next Release</dd>
-    <dd class="f3 f2-ns b ml0">10-22</dd>
+  <dl className="dib mr5">
+    <dd className="f6 f5-ns b ml0">Next Release</dd>
+    <dd className="f3 f2-ns b ml0">10-22</dd>
   </dl>
-  <dl class="dib mr5">
-    <dd class="f6 f5-ns b ml0">Days Left</dd>
-    <dd class="f3 f2-ns b ml0">4</dd>
+  <dl className="dib mr5">
+    <dd className="f6 f5-ns b ml0">Days Left</dd>
+    <dd className="f3 f2-ns b ml0">4</dd>
   </dl>
-  <dl class="dib mr5">
-    <dd class="f6 f5-ns b ml0">Favorite Cat</dd>
-    <dd class="f3 f2-ns b ml0">All of Them</dd>
+  <dl className="dib mr5">
+    <dd className="f6 f5-ns b ml0">Favorite Cat</dd>
+    <dd className="f3 f2-ns b ml0">All of Them</dd>
   </dl>
-  <dl class="dib">
-    <dd class="f6 f5-ns b ml0">App Downloads</dd>
-    <dd class="f3 f2-ns b ml0">1,200</dd>
+  <dl className="dib">
+    <dd className="f6 f5-ns b ml0">App Downloads</dd>
+    <dd className="f3 f2-ns b ml0">1,200</dd>
   </dl>
 </article>
-<article class="pa3 pa5-ns" data-name="slab-stat-small">
-  <h3 class="f6 ttu tracked">{date}</h3>
-  <div class="cf">
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Sessions Used</dd>
-      <dd class="f3 fw6 ml0">{sessionsUsed}</dd>
+<article className="pa1 pa5-ns" data-name="slab-stat-small">
+  <h3 className="f6 ttu tracked">{date}</h3>
+  <div className="cf">
+    
+     <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+      <dd className="f6 fw4 ml0">{date}</dd>
+      <dd className="f3 fw6 ml0"></dd>
     </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Sessions Left</dd>
-      <dd class="f3 fw6 ml0">{sessionsTotal-sessionsUsed}</dd>
+    <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+      <dd className="f6 fw4 ml0">Weight</dd>
+      <dd className="f3 fw6 ml0">93</dd>
     </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Next Release</dd>
-      <dd class="f3 fw6 ml0">10-22</dd>
+    <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+      <dd className="f6 fw4 ml0">Muscle Mass</dd>
+      <dd className="f3 fw6 ml0">16</dd>
     </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Days Left</dd>
-      <dd class="f3 fw6 ml0">4</dd>
+    <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+      <dd className="f6 fw4 ml0">Fat Level</dd>
+      <dd className="f3 fw6 ml0">14</dd>
     </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Favorite Cat</dd>
-      <dd class="f3 fw6 ml0">All of Them</dd>
+    <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+      <dd className="f6 fw4 ml0">BMI</dd>
+      <dd className="f3 fw6 ml0">30.1</dd>
     </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title">
-      <dd class="f6 fw4 ml0">App Downloads</dd>
-      <dd class="f3 fw6 ml0">1,200</dd>
+    <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+      <dd className="f6 fw4 ml0">Fat Level Organs</dd>
+      <dd className="f3 fw6 ml0">13</dd>
+    </dl>
+    <dl className="fl fn-l w-50 dib-l w-auto-l lh-title">
+      <dd className="f6 fw4 ml0">App Downloads</dd>
+      <dd className="f3 fw6 ml0">1,200</dd>
     </dl>
   </div>
 </article>
