be0bfb43ac0c8bf1da299766e6038a5925e20303
Ken Crites <kenrcrites@gmail.com>
2019-04-18 19:56:26 +0200

    Updated logo to DMPT

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import boat from './boat.png';
+import logo from './dmpt.jpg';
 
 
 const Navigation = ({onRouteChange, isSignedIn}) => {
 	if(isSignedIn) {
 		return(
 			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-				<img className='ma4 mt0 br2' style={{marginRight: 'auto', height: '60px', justifyContent: 'flex-start', paddingTop: '10px'}} alt='Logo' src={boat} />
+				<img className='ma4 mt0 br2' style={{marginRight: 'auto', height: '60px', justifyContent: 'flex-start', paddingTop: '10px'}} alt='Logo' src={logo} />
 				<p onClick={() => onRouteChange('home')} className='f5 link dim black  pa3 pointer'>Home</p>
 				<p onClick={() => onRouteChange('trainingHistory')} className='f5 link dim black  pa3 pointer'>Training History</p>
 				<p onClick={() => onRouteChange('stats')} className='f5 link dim black  pa3 pointer'>User Stats</p>
@@ -17,7 +17,7 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
 		} else {
 				return(
 				<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-					<img className='ma4 mt0 br2' style={{marginRight: 'auto', height: '60px', justifyContent: 'flex-start', paddingTop: '10px'}} alt='Logo' src={boat} />
+					<img className='ma4 mt0 br2' style={{marginRight: 'auto', height: '60px', justifyContent: 'flex-start', paddingTop: '10px'}} alt='Logo' src={logo} />
 					<p onClick={() => onRouteChange('signin')} className='f5 link dim black  pa3 pointer'>Sign in</p>
 					<p onClick={() => onRouteChange('register')} className='f5 link dim black  pa3 pointer'>Register</p>
 				</nav>
